b39263e98f8f0af5248a842ac5e24154ad356714
JulissaDantes <julissadcj@gmail.com>
2016-04-03 11:01:12 -0430

    Con las imagenes cargadas y listas para usar

--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,9 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
+    Rambo : "journey_1.png",
+    Terrorist2: "RenderPedroLOAD68.png",
+    explosion: "Atomic_Explosion.png",
+    bomba: "13052336391841183542psd-black-bomb-icon.png"
 };
 
 var g_resources = [];
